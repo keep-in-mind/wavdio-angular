5737ef45a9c62aa36f2f5fad1fc8e5f9c60c8f79
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-01-07 16:33:36 +0100

    Use setting service in setting component

--- src/app/admin/settings/settings.component.ts
+++ src/app/admin/settings/settings.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {CookielawService} from '../../services/cookielaw.service';
+import {SettingService} from '../../services/setting.service';
+import {AuthenticationService} from '../../services/authentification.service';
+import {Router} from '@angular/router';
+import {Setting} from '../../models/setting';
 
 @Component({
   selector: 'app-settings',
@@ -8,12 +12,24 @@ import {CookielawService} from '../../services/cookielaw.service';
 })
 export class SettingsComponent implements OnInit {
 
+  setting: Setting;
+
   descHeader = 'Einstellungen';
 
   constructor(
+    private router: Router,
+    private settingService: SettingService,
+    private auth: AuthenticationService,
     public cookieLawService: CookielawService) {
   }
 
   ngOnInit() {
+    if (!this.auth.isLoggedIn()) {
+      this.router.navigate(['/admin']);
+    } else {
+      this.settingService.getSettings().subscribe(
+        settings => this.setting = settings[0]
+      );
+    }
   }
 }
