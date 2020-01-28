ce6e24ab95a68d5da24f47abf14bbf5c4296fd29
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-01-28 18:36:04 +0100

    Remove unnecessary museum variables and methods

--- src/app/user/infopage/infopage.component.ts
+++ src/app/user/infopage/infopage.component.ts
@@ -2,11 +2,8 @@ import {Component, Inject, Input, LOCALE_ID, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 
 import {Infopage} from '../../models/infopage';
-import {Museum} from '../../models/museum';
-import {MuseumContent} from '../../models/museum-content';
 import {CookielawService} from '../../services/cookielaw.service';
 import {InfopageService} from '../../services/infopage.service';
-import {MuseumService} from '../../services/museum.service';
 
 @Component({
   selector: 'app-infopage',
@@ -16,13 +13,11 @@ import {MuseumService} from '../../services/museum.service';
 export class InfopageComponent implements OnInit {
 
   infopage: Infopage;
-  museum: Museum;
 
   constructor(
     @Inject(LOCALE_ID) public locale: string,
     private infopageService: InfopageService,
     private route: ActivatedRoute,
-    private museumService: MuseumService,
     public cookieLawService: CookielawService,
     public router: Router) {
   }
@@ -37,36 +32,6 @@ export class InfopageComponent implements OnInit {
           infopage => this.infopage = infopage
         );
       });
-      this.museumService.getMuseums().subscribe(
-        museums => {
-          this.museum = museums[0];
-        }
-      );
     }
-  }
-
-  getMuseumContent(lang: String): MuseumContent {
-
-    /* return localized content */
-
-    for (const content of this.museum.contents) {
-      if (content.lang === lang) {
-        return content;
-      }
-    }
-
-    /* not available ? fall back to German */
-
-    console.warn('No localized content available for locale ' + lang);
-
-    for (const content of this.museum.contents) {
-      if (content.lang === 'de') {
-        return content;
-      }
-    }
-
-    /* not available ? must not happen. has to be created when constructing exposition */
-
-    console.error('No German fallback content available');
   }
 }
