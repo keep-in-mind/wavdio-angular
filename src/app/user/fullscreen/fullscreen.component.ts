61a6b64a32bf82f4d6245e428c654204f87f81fc
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-03-10 15:59:58 +0100

    Copy carousel code to fullscreen component

--- src/app/user/fullscreen/fullscreen.component.ts
+++ src/app/user/fullscreen/fullscreen.component.ts
@@ -1,4 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, Input, LOCALE_ID, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+import {CookielawService} from '../../services/cookielaw.service';
+import {Exhibit} from '../../models/exhibit';
+import {ExhibitService} from '../../services/exhibit.service';
+import {Image} from '../../models/image';
+import {ExhibitContent} from "../../models/exhibit-content";
 
 @Component({
   selector: 'app-fullscreen',
@@ -7,9 +14,45 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FullscreenComponent implements OnInit {
 
-  constructor() {
+  slideConfig = {
+    dots: true,
+    prevArrow: '#prevSlide',
+    nextArrow: '#nextSlide',
+  };
+
+  exhibit: Exhibit;
+
+  constructor(
+    @Inject(LOCALE_ID) public locale: string,
+    private activatedRoute: ActivatedRoute,
+    private exhibitService: ExhibitService,
+    public router: Router,
+    public cookieLawService: CookielawService) {
   }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      const exhibitId = params.id;
+
+      this.exhibitService.getExhibit(exhibitId).subscribe(exhibit => {
+          this.exhibit = exhibit;
+        }
+      );
+    });
+  }
+
+  getExhibitContent(lang: String): ExhibitContent {
+
+    /* return localized content */
+
+    for (const content of this.exhibit.contents) {
+      if (content.lang === lang) {
+        return content;
+      }
+    }
+
+    /* not available ? must not happen. has to be created when constructing exhibit */
+
+    console.error(`No content available for ${lang}`);
   }
 }
