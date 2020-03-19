6260dc085860751378599648157400d2bcf1fa78
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-03-19 18:18:26 +0100

    Create title, lang and description fields

--- src/app/admin/admin-museum/admin-museum.component.ts
+++ src/app/admin/admin-museum/admin-museum.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, LOCALE_ID, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 
 import {AuthenticationService} from '../../services/authentification.service';
@@ -14,11 +14,28 @@ export class AdminMuseumComponent implements OnInit {
 
   museum: Museum;
 
+  languages = ['de', 'en', 'es', 'fr'];
+  selectedLanguage = this.locale;
+
   alertType: number;
   alertMessage: string;
   showAlert: boolean;
 
+  placeholder =
+    'Dieser Text kann mit Markdown formattiert werden.\n' +
+    '\n' +
+    'Leere Zeilen bewirken Absätze. Text kann *kursiv* oder **fett** gedruckt werden. ' +
+    '[Links](www.google.de) auf externe Seiten sind ebenfalls möglich.\n' +
+    '\n' +
+    '### Unterkapitel\n' +
+    '\n' +
+    'Darüber hinaus sind viele weitere Formattierungen möglich (de.wikipedia.org/wiki/Markdown) wie zum Beispiel:\n' +
+    '- Aufzählungen\n' +
+    '- Tabellen\n' +
+    '- Zitate';
+
   constructor(
+    @Inject(LOCALE_ID) private locale: string,
     private activatedRoute: ActivatedRoute,
     private auth: AuthenticationService,
     private router: Router,
@@ -35,6 +52,56 @@ export class AdminMuseumComponent implements OnInit {
     this.museumService.getMuseums().subscribe(museums => {
       this.museum = museums[0];
     });
+  }
+
+  updateExposition() {
+    if (!this.getMuseumContent(this.selectedLanguage).name) {
+      this.showAlertMessage(3, 5,
+        'Das Titelfeld darf nicht leer sein. Bitte korrigieren Sie Ihre Eingabe.');
+      return;
+    }
+
+    if (this.getMuseumContent(this.selectedLanguage).name.startsWith(' ')) {
+      this.showAlertMessage(3, 5,
+        'Das Titelfeld darf keine vorangestellten Leerzeichen beinhalten. ' +
+        'Bitte korrigieren Sie Ihre Eingabe.');
+      return;
+    }
+
+    this.museumService.updateMuseum(this.museum).subscribe(res => {
+        if (this.museumService.errorCode === 500) {
+          this.showAlertMessage(3, 5, 'Es ist ein Fehler aufgetreten. ' +
+            'Mehr Informationen finden Sie in den Serverlogs.');
+        } else {
+          this.showAlertMessage(0, 5, 'Ihre Änderungen wurden erfolgreich übernommen');
+        }
+      }
+    );
+  }
+
+  deleteExposition() {
+    if (confirm('Sind Sie sicher, dass Sie diese Ausstellung löschen möchten? ' +
+      'Alle Ausstellungsstücke und die dazugehörigen Medien gehen dabei ebenfalls unwiderruflich verloren.')) {
+
+      this.museumService.deleteMuseum(this.museum).subscribe(res => {
+        this.router.navigate(['/admin/home']);
+      }, (err) => {
+        console.log(err);
+        this.showAlertMessage(3, 5,
+          'Da ist etwas schief gegangen. Prüfen Sie bitte erneut Ihre Eingabedaten.');
+      });
+    }
+  }
+
+  getMuseumContent(locale: string) {
+    for (const content of this.museum.contents) {
+      if (content.lang === locale) {
+        return content;
+      }
+    }
+
+    // not available ? must not happen. has to be created when constructing museum
+    console.error(`MuseumContent missing for locale ${locale}`);
   }
 
   private showAlertMessage(type: number, seconds: number, message: string) {
