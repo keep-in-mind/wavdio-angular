29a16123dfb54476203cf38dbfb317a581441dc4
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-03-20 15:59:18 +0100

    Include language selection

--- src/app/admin/admin-header/admin-header.component.ts
+++ src/app/admin/admin-header/admin-header.component.ts
@@ -1,4 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Inject, Input, LOCALE_ID, OnInit} from '@angular/core';
+import {Setting} from "../../models/setting";
+import {Router} from "@angular/router";
+import {SettingService} from "../../services/setting.service";
 
 class Breadcrumb {
   text: string;
@@ -19,9 +22,26 @@ export class AdminHeaderComponent implements OnInit {
 
   @Input() breadcrumbs: Breadcrumb[];
 
-  constructor() {
+  setting: Setting;
+
+  // Same alt texts used multiple times
+  langAltTexts = {
+    de: 'German Flag Icon',
+    en: 'British Flag Icon',
+    es: 'Spanish Flag Icon',
+    fr: 'French Flag Icon'
+  };
+
+  constructor(
+    @Inject(LOCALE_ID) public locale: string,
+    public router: Router,
+    private settingService: SettingService) {
   }
 
   ngOnInit() {
+    this.settingService.getSettings().subscribe(settings => {
+      console.log(settings);
+      this.setting = settings[0];
+    });
   }
 }
